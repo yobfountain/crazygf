2be395cd767df51369e6967bb27464a17553747d
Raquel Hernandez <maggit@maggit.net>
2012-09-08 15:23:34 -0400

    Adding rails_admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Crazygf::Application.routes.draw do
+  mount RailsAdmin::Engine => '/cgf_admin', :as => 'rails_admin'
+
+  devise_for :admins
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
