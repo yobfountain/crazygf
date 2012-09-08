1c35ccf357a6e7835ba8ddab3de11cce4f8d5da0
Raquel Hernandez <maggit@maggit.net>
2012-09-08 16:33:40 -0400

    Adding acts_as_taggable gem

--- app/models/text.rb
+++ app/models/text.rb
@@ -1,6 +1,7 @@
 class Text < ActiveRecord::Base
   attr_accessible :category_id, :score, :text, :image_url
   attr_accessible :picture
+  belongs_to :category
   
   has_attached_file :picture,
       :storage => :s3,
