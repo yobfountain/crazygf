2dda1ccf34492d441681bcebd44788dd492b7e15
Raquel Hernandez <maggit@maggit.net>
2012-09-09 01:02:02 -0400

    some pg logic

--- db/migrate/20120909043355_add_enabled_to_user.rb
+++ db/migrate/20120909043355_add_enabled_to_user.rb
@@ -1,5 +1,5 @@
 class AddEnabledToUser < ActiveRecord::Migration
   def change
-    add_column :users, :enabled, :boolean, :default => 1
+    add_column :users, :enabled, :boolean, :default => true
   end
 end
