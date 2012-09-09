989a26c0a76138b007c3b363404ea46f226f1466
Raquel Hernandez <maggit@maggit.net>
2012-09-09 13:52:16 -0400

    Adding full search

--- app/models/dynamic_text.rb
+++ app/models/dynamic_text.rb
@@ -3,4 +3,12 @@ class DynamicText < ActiveRecord::Base
   belongs_to :category
   has_many :dynamic_text_responses
   
+  def self.search(search)
+    if search
+      search_length = search.split.length
+      find(:first, :conditions => [(['keywords LIKE ?'] * search_length).join(' OR ')] + search.split.map { |name| "%#{name}%" },:order => 'RANDOM()')
+    else
+      nil
+    end
+  end
 end
